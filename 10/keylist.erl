69bc27305bcf31d9f25cad950901bbc36f92c25f
ValeryDanilchenko <valerius022@gmail.com>
2023-04-06 02:35:51 +0700

    HW #10 v0.1

---
+++ 10/keylist.erl
@@ -0,0 +1,114 @@
+-module(keylist).
+-export([loop/1, init/1, terminate/0 ]).
+-export([add/4, is_member/2, take/2, find/2, delete/2, start/1, start_link/1, stop/1]).
+
+-record(state, {list = [], counter = 0}).
+
+%% @doc API function thats register new process and starts main loop
+-spec(init(atom()) -> 
+    no_return()).
+init(Name) ->
+    register(Name, self()),
+    loop(#state{}).
+
+%% @doc API function spawning new monitored process
+-spec(start(Name :: atom()) -> 
+    {Pid :: pid(), MonitorRef :: reference()}).
+start(Name) ->
+    Pid = spawn(?MODULE, init, [#state{}]),
+    register(Name, Pid),
+    MonitorRef = erlang:monitor(process, Pid),
+    {ok, Pid, MonitorRef}.
+
+
+%% @doc API function spawning new linked process
+-spec(start_link(Name :: atom()) ->
+    Pid :: pid()).
+start_link(Name) ->
+    Pid = spawn_link(?MODULE, init, [#state{}]),
+    register(Name, Pid),
+    Pid.
+
+%% @doc API function to exit process
+-spec(terminate() -> 
+    ok).
+terminate() ->
+    ok.
+
+%% @doc API function that stops main process
+-spec(stop(Name :: atom()) ->
+    ok).
+stop(Name)->
+    Name ! stop,
+    ok.
+
+%% @doc API function thats add new element to the state
+-spec(add(Name :: atom(), Key :: atom() | string(), Value :: atom() | string(), Comment :: atom() | string()) ->
+    ok).
+add(Name, Key, Value, Comment) -> 
+    Name ! {self(), add, Key, Value, Comment},
+    ok.
+
+%% @doc API function thats checking if element is member of the state
+-spec(is_member(Name :: atom(), Key :: atom()| string()) ->
+    ok).
+is_member(Name, Key)->
+    Name ! {self(), is_member, Key},
+    ok.
+
+%% @doc API function that returns element`s data by is`s key and deletes it from state
+-spec(take(Name :: atom(), Key :: atom()| string()) ->
+    ok).
+take(Name, Key)-> 
+    Name ! {self(), take, Key},
+    ok.
+
+%% @doc API function that returns element`s data by is`s key
+-spec(find(Name :: atom(), Key :: atom()| string()) ->
+    ok).
+find(Name, Key) -> 
+    Name ! {self(), find, Key},
+    ok.
+
+%% @doc API function that deletes the element from state
+-spec(delete(Name :: atom(), Key :: atom()| string()) ->
+    ok).
+delete(Name, Key)->
+    Name ! {self(), delete, Key},
+    ok.
+
+
+
+%%%%%% PRIVATE FUNCTIONS %%%%%%
+
+%% @doc The main loop that sincronously handle the commands from manager
+-spec(loop(#state{list :: list(), counter :: non_neg_integer()}) ->
+    ok).
+loop(#state{list = List, counter = Counter} = State) ->
+    receive
+        {From, add, Key, Value, Comment} ->
+            NewState = State#state{list = [{Key, Value, Comment} | List], counter = Counter + 1},
+            From ! {ok, NewState},
+            loop(NewState);
+        {From, is_member, Key} ->
+            Result = lists:keymember(Key, 1, List),
+            NewState = State#state{counter = Counter + 1},
+            From ! {Result, NewState},
+            loop(NewState);
+        {From, take, Key} ->
+            {_, Element, NewList} = lists:keytake(Key, 1, List),
+            NewState = State#state{list = NewList, counter = Counter + 1},
+            From ! {Element, NewState},
+            loop(NewState);
+        {From, find, Key} ->
+            Element = lists:keyfind(Key, 1, List),
+            NewState = State#state{counter = Counter + 1},
+            From ! {Element, NewState},
+            loop(NewState);
+        {From, delete, Key} ->
+            NewState = State#state{list = lists:keydelete(Key, 1, List), counter = Counter + 1},
+            From ! {ok, NewState},
+            loop(NewState); 
+        stop ->
+            keylist:terminate()
+    end.
---
+++ 10/keylist_mgr.erl
@@ -0,0 +1,148 @@
+%%  @doc process menager
+-module(keylist_mgr).
+
+%% @type map includes info about starting process: it`s name and is it permonentor not
+%% if it is we will restrt it by the time it crash
+-type(parameters() :: #{
+    name => atom(),
+    restart => permanent | temporary
+    }).
+
+%% Callback
+-export([loop/1,  init/0, terminate/1]).
+%% API funs
+-export([ start/0, start_child/1, stop_child/1, stop/0, get_names/0]). 
+
+-record(state, {
+        children = []   :: list({atom(), pid()}),
+        permanent = []  :: list(pid())
+    }).
+
+
+%% @doc API function thats register keylist_mgr process, set process_flag and start the main loop
+-spec(init() -> 
+    no_return()).
+init() ->
+    process_flag(trap_exit, true),
+    register(?MODULE, self()),
+    loop(#state{}).
+
+%% @doc API function starting new monitor process
+-spec(start() -> 
+    {ok, Pid :: pid(), MonitorRef :: reference()}).
+start() ->
+    {Pid, MonitorRef} = spawn_monitor(?MODULE, init, [#state{}]),
+    {ok, Pid, MonitorRef}.
+
+
+
+%% @doc API function for stop process manager and stop all child processes
+-spec(terminate(#state{children :: list(), permanent :: list()}) -> 
+    ok).
+terminate(#state{children = _Children} = State) ->
+    lists:foreach(
+                fun({Name, _Pid}) ->
+                    keylist:stop(Name)
+                end,
+                State#state.children),
+                ok.
+
+%% @doc API function sending message to main process initialising starting the child process
+-spec(start_child(parameters()) ->
+    none() | {error, atom()}).
+start_child(Params) ->
+    case Params of
+        #{name := _Name , restart := _Restart} ->
+            keylist_mgr ! {self(), start_child, Params};
+        _ ->
+            {error, badarg}
+    end.
+
+    
+ 
+%% @doc API function sending message to main process which stops this child process
+-spec(stop_child(Name :: atom()) -> none()).    
+stop_child(Name) ->
+    keylist_mgr ! {self(), stop_child, Name},
+    ok.
+
+
+
+%% @doc API function sending message 'stop' to the main process which stops all the children and itself 
+-spec(stop() -> none()).
+stop() ->
+    keylist_mgr ! stop,
+    ok.
+
+
+%% @doc API function sending message to the main process which sends info about child processes
+-spec(get_names() -> none()).
+get_names() ->
+    keylist_mgr ! {self(), get_names},
+    ok.
+
+
+%%%%%% PRIVATE FUNCTIONS %%%%%%
+
+
+%% @doc the main function that process commands incoming with mesages from API functions
+%% can spawn_link new processes, delete processes, give names of already running processes, restart or log exit processes or exit main process keylist_mgr
+%% it records process names and parameters into the #state
+-spec(loop(#state{children :: list({string(), pid()}), permanent :: list(pid())}) ->
+    string() | none()).
+loop(#state{children = Children, permanent = Permanent} = State) ->
+    receive
+        {From, start_child, #{name := Name, restart := Restart}}  ->
+            case proplists:is_defined(Name, Children) of
+                {Name, _Pid} ->
+                    io:format("Process ~p is alredy started  ~n",[Name]),
+                    From ! {error, already_started},
+                    loop(State);
+                false ->
+                    Pid = keylist:start_link(Name),
+                    case Restart of
+                        permanent -> 
+                            NewState = State#state{children = [{Name, Pid} | Children], permanent = [Pid | Permanent]};
+                        temporary ->
+                            NewState = State#state{children = [{Name, Pid} | Children], permanent = Permanent}
+                    end,
+                    From ! {ok, Pid},
+                    loop(NewState)
+            end;
+        {From, stop_child, Name} ->
+            case proplists:is_defined(Name, Children) of
+                true ->
+                    keylist:stop(Name),
+                    NewState = State#state{children = proplists:delete(Name, Children),
+                            permanent = lists:delete(whereis(Name), Permanent)},
+                    From ! {ok, NewState},
+                    loop(NewState);
+                false ->
+                    From ! {error, undefined},
+                    loop(State)
+            end;
+         stop ->
+            keylist_mgr:terminate(State);
+        {From, get_names} ->
+            Names = [Name || {Name, _Pid} <- Children],
+            From ! {ok, lists:reverse(Names)},
+            loop(State);
+        {'EXIT', Pid, Reason} ->
+            case lists:keyfind(Pid, 2, Children) of
+                {Name, Pid} ->
+                    case lists:member(Pid, Permanent) of
+                        true -> 
+                            NewPid = keylist:start_link(Name),
+                            NewState = State#state{children = [{Name, NewPid} | proplists:delete(Name, Children)], 
+                                    permanent = [NewPid | lists:delete(Pid, Permanent)]},
+                            io:format("Process ~p 'DOWN' with reason: ~p. Restared with PID ~p~n", [Pid, Reason, NewPid]);
+                        false ->
+                            NewState = State#state{children = proplists:delete(Name, Children), permanent = Permanent},
+                            io:format("Process ~p 'DOWN' with reason: ~p~n",[Pid, Reason])
+                    end,
+                    loop(NewState);
+                false ->
+                    loop(State)
+            end
+    end.
+
